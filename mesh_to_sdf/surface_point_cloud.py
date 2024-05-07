fix std (numpy) variance (deepsdf paper)

mesh_to_sdf/surface_point_cloud.py
@@ -106,8 +106,8 @@ class SurfacePointCloud:
         query_points = []
         surface_sample_count = int(number_of_points * 47 / 50) // 2
         surface_points = self.get_random_surface_points(surface_sample_count, use_scans=use_scans)
-        query_points.append(surface_points + np.random.normal(scale=0.0025, size=(surface_sample_count, 3)))
-        query_points.append(surface_points + np.random.normal(scale=0.00025, size=(surface_sample_count, 3)))
+        query_points.append(surface_points + np.random.normal(scale=math.sqrt(0.0025), size=(surface_sample_count, 3)))
+        query_points.append(surface_points + np.random.normal(scale=math.sqrt(0.00025), size=(surface_sample_count, 3)))
         
         unit_sphere_sample_count = number_of_points - surface_points.shape[0] * 2
         unit_sphere_points = sample_uniform_points_in_unit_sphere(unit_sphere_sample_count)